f6dc537a0bcb6169542c51751b1e220f654d0d43
Tia Firing <tia.firing@nav.no>
2021-08-31 14:27:51 +0200

    fjerner migreringslogikk

--- src/main/kotlin/no/nav/syfo/sykmeldingvarsel/kafka/SendtSykmeldingConsumerService.kt
+++ src/main/kotlin/no/nav/syfo/sykmeldingvarsel/kafka/SendtSykmeldingConsumerService.kt
@@ -3,12 +3,8 @@ package no.nav.syfo.sykmeldingvarsel.kafka
 import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.log
 import no.nav.syfo.sykmeldingvarsel.SendtSykmeldingVarselService
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.time.Duration
-import java.time.LocalDate
-import java.time.OffsetDateTime
-import java.time.ZoneOffset
 
 class SendtSykmeldingConsumerService(
     private val kafkaConsumer: KafkaConsumer<String, SendtSykmelding>,
@@ -22,25 +18,13 @@ class SendtSykmeldingConsumerService(
         while (applicationState.ready) {
             kafkaConsumer.poll(Duration.ZERO).forEach {
                 try {
-                    if (skalBehandle(it)) {
-                        log.info("Mottatt sendt sykmelding med id ${it.value().kafkaMetadata.sykmeldingId}")
-                        sendtSykmeldingVarselService.handterSendtSykmelding(it.value())
-                    }
+                    log.info("Mottatt sendt sykmelding med id ${it.value().kafkaMetadata.sykmeldingId}")
+                    sendtSykmeldingVarselService.handterSendtSykmelding(it.value())
                 } catch (ex: Exception) {
                     log.error("Error in consuming sendt sykmelding", ex)
                     throw ex
                 }
             }
         }
-    }
-
-    fun skalBehandle(cr: ConsumerRecord<String, SendtSykmelding>): Boolean {
-        val partisjonOffset = mapOf(0 to 6587214, 1 to 6578122, 2 to 6588686)
-        if (cr.offset() > partisjonOffset[cr.partition()]!!) {
-            if (cr.value().kafkaMetadata.timestamp.isAfter(OffsetDateTime.of(LocalDate.of(2021, 8, 31).atStartOfDay(), ZoneOffset.UTC))) {
-                return true
-            }
-        }
-        return false
     }
 }
