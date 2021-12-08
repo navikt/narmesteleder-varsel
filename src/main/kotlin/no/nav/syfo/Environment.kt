2d0326edb2c781b32b66a91ecc20238e37c2947b
Tia Firing <tia.firing@nav.no>
2021-12-08 08:28:16 +0100

    leser sendte sykmeldinger fra aiven

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,8 +1,5 @@
 package no.nav.syfo
 
-import no.nav.syfo.kafka.KafkaConfig
-import no.nav.syfo.kafka.KafkaCredentials
-
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
     val applicationName: String = getEnvVar("NAIS_APP_NAME", "narmesteleder-varsel"),
@@ -11,28 +8,17 @@ data class Environment(
     val dbHost: String = getEnvVar("NAIS_DATABASE_HOST"),
     val dbPort: String = getEnvVar("NAIS_DATABASE_PORT"),
     val dbName: String = getEnvVar("NAIS_DATABASE_DATABASE"),
-    override val cluster: String = getEnvVar("NAIS_CLUSTER_NAME"),
-    override val truststore: String? = getEnvVar("NAV_TRUSTSTORE_PATH"),
-    override val truststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD"),
-    override val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
     val narmesteLederLeesahTopic: String = "teamsykmelding.syfo-narmesteleder-leesah",
-    val sendtSykmeldingKafkaTopic: String = "syfo-sendt-sykmelding",
+    val sendtSykmeldingKafkaTopic: String = "teamsykmelding.syfo-sendt-sykmelding",
     val doknotifikasjonTopic: String = "teamdokumenthandtering.privat-dok-notifikasjon-med-kontakt-info",
     val schemaRegistryUrl: String = getEnvVar("KAFKA_SCHEMA_REGISTRY"),
     val kafkaSchemaRegistryUsername: String = getEnvVar("KAFKA_SCHEMA_REGISTRY_USER"),
     val kafkaSchemaRegistryPassword: String = getEnvVar("KAFKA_SCHEMA_REGISTRY_PASSWORD")
-) : KafkaConfig {
+) {
     fun jdbcUrl(): String {
         return "jdbc:postgresql://$dbHost:$dbPort/$dbName"
     }
 }
-
-data class VaultSecrets(
-    val serviceuserUsername: String = getEnvVar("SERVICEUSER_USERNAME"),
-    val serviceuserPassword: String = getEnvVar("SERVICEUSER_PASSWORD"),
-    override val kafkaPassword: String = serviceuserPassword,
-    override val kafkaUsername: String = serviceuserUsername
-) : KafkaCredentials
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
     System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
